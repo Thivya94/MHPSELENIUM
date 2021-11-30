e69789c013f9cb5fa47d7c69061afb3932ccfb85
Thivya94 <thivyanagammai1994@gmail.com>
2021-11-30 16:46:10 +0530

    Mefrstcommit

---
+++ src/main/java/MHP_GRP/MHP_ARCHI/App.java
@@ -0,0 +1,13 @@
+package MHP_GRP.MHP_ARCHI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MHP_GRP/MHP_ARCHI/AppTest.java
@@ -0,0 +1,20 @@
+package MHP_GRP.MHP_ARCHI;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
